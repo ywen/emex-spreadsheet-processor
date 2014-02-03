d82003da22e02d23a668fd15e9ca41ff021cbcd4
Yi Wen <ywen.github@gmail.com>
2014-02-02 23:25:44 -0600

    First commit

---
+++ lib/emex-spreadsheet-processor.rb
@@ -0,0 +1,11 @@
+Dir["#{File.dirname(__FILE__)}/emex-spreadsheet-processor/**/*.rb"].each do |f| 
+  require f
+end
+
+module Emex
+  module Spreadsheet
+    module Processor
+      # Your code goes here...
+    end
+  end
+end
